24301bccb43792cd698b048d18096fe94207148d
Jim7rfp <Jim7fp@gmail.com>
2021-09-22 17:36:06 +0300

    22.09.2021 Финал переделки сайта (без реализации листания нескольких картинок в одном блоге новостей)

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -23,15 +23,13 @@ $sys_phone = Yii::$app->params['sys_phone'];
 				<span>Охотничий клуб «Трофей» был создан группой энтузиастов. Мы организуем и проводим охоту и рыбалку по Республике Татарстан.</span>
 			</div>
 
-			<div class="club-mission">
-				<div class="club-mission__img"></div>
-				<div class="club-mission__discr">
-					<p class="club-mission__discr-text text18 text-bold">
-					Миссия клуба:
-					<br>– Обеспечение бережного отношения к фауне, следовательно, процветания фауны и флоры Республики
-					Татарстан;
-					<br>– Защита зверей и дикой природы от браконьеров и хозяйственной деятельности человека.
-					</p>
+			<div class="blg">
+				<div class="blg__img"></div>
+				<div class="blg__text">
+					<ul>Миссия клуба:
+						<li>Обеспечение бережного отношения к фауне, следовательно, процветания фауны и флоры Республики Татарстан</li>
+						<li>Защита зверей и дикой природы от браконьеров и хозяйственной деятельности человека</li>
+					</ul>
 				</div>
 			</div>
 
@@ -48,19 +46,17 @@ $sys_phone = Yii::$app->params['sys_phone'];
 			</div>
 
 			<div class="info-menu">
-				<button class="info-menu__btn1 button my-btn">ОБРАТНАЯ СВЯЗЬ</button>
-				<a class="info-menu__btn2 button" href="info">ДОКУМЕНТЫ</a>
-				<a class="info-menu__btn1 button" target="_blank" href="https://ojm.tatarstan.ru/kalendar-ohotnika-tatarstan-2020-sroki-ohoti-na.htm">КАЛЕНДАРЬ ОХОТНИКА ТАТАРСТАН 2021</a>
+				<div class="info-menu-block">
+					<button class="info-menu__btn1 button my-btn">ОБРАТНАЯ СВЯЗЬ</button>
+					<a class="info-menu__btn2 button" href="info">ДОКУМЕНТЫ</a>
+					<a class="info-menu__btn1 button" target="_blank" href="https://ojm.tatarstan.ru/kalendar-ohotnika-tatarstan-2020-sroki-ohoti-na.htm">КАЛЕНДАРЬ ОХОТНИКА ТАТАРСТАН 2021</a>
+				</div>
 			</div>
 		</div>
 
 		<article>
 			<div class="blog">
-				<div class="blog__img">
-					<figure>
-						<img src="/images/scolopax.jpg" alt="">
-					</figure>
-				</div>
+				<div class="blog__img"><figure><img src="/images/scolopax.jpg" alt=""></figure></div>
 				<div class="blog_txt">
 					<h3>Информация</h3><h4>Вниманию охотников!</h4>
 					<p>С 4 сентября по 5 января открыт сезон охоты на боровую дичь.</p>
@@ -69,11 +65,7 @@ $sys_phone = Yii::$app->params['sys_phone'];
 			</div>
 
 			<div class="blog">
-				<div class="blog__img">
-					<figure>
-						<img src="/images/anas.jpg" alt="">
-					</figure>
-				</div>
+				<div class="blog__img"><figure><img src="/images/anas.jpg" alt=""></figure></div>
 				<div class="blog_txt">
 					<h3>Информация</h3><h4>Вниманию охотников!</h4>
 					<p>С 4 сентября по 5 декабря открывается сезон охоты на водоплавающую, болотно-луговую, степную и полевую дичь.</p>
@@ -82,11 +74,7 @@ $sys_phone = Yii::$app->params['sys_phone'];
 			</div>
 
 			<div class="blog">
-				<div class="blog__img">
-					<figure>
-						<img src="/images/ducks.jpg" alt="">
-					</figure>
-				</div>
+				<div class="blog__img"><figure><img src="/images/ducks.jpg" alt=""></figure></div>
 				<div class="blog_txt">
 					<h3>Информация</h3><h4>Вниманию охотников!</h4>
 					<p>С 17 по 26 апреля открывается весенний сезон охоты на пернатую дичь.</p>
@@ -99,7 +87,9 @@ $sys_phone = Yii::$app->params['sys_phone'];
 
 		<div class="info-block">
 			<div class="info-menu">
+			<div class="info-menu-block">
 				<a class="info-menu__btn1 button" href="news">Все новости</a>
+			</div>
 			</div>
 		</div>
 	</div>
@@ -112,51 +102,59 @@ $sys_phone = Yii::$app->params['sys_phone'];
 
 			<div class="animals-gallery">
 				<div class="animals-gallery-item">
-					<a class="gallery-item__img" href=""></a>
-					<img class="gallery-item__img" src="/images/fauna_rt/boar.png" alt="">
-					<p class="gallery-item__description">Кабан</p>
+					<a class="gallery-item__img" href="hunt#boar">
+						<img class="gallery-item__img" src="/images/fauna_rt/boar.png" alt="">
+						<p class="gallery-item__description">Кабан</p>
+					</a>
 				</div>
 
 				<div class="animals-gallery-item">
-					<a class="gallery-item__img" href=""></a>
-					<img class="gallery-item__img" src="/images/fauna_rt/elk.png" alt="">
-					<p class="gallery-item__description">Лось</p>
+					<a class="gallery-item__img" href="hunt#elk">
+						<img class="gallery-item__img" src="/images/fauna_rt/elk.png" alt="">
+						<p class="gallery-item__description">Лось</p>
+					</a>
 				</div>
 
 				<div class="animals-gallery-item">
-					<a class="gallery-item__img" href=""></a>
-					<img class="gallery-item__img" src="/images/fauna_rt/hare.png" alt="">
-					<p class="gallery-item__description">Заяц</p>
+					<a class="gallery-item__img" href="hunt#hare">
+						<img class="gallery-item__img" src="/images/fauna_rt/hare.png" alt="">
+						<p class="gallery-item__description">Заяц</p>
+					</a>
 				</div>
 
 				<div class="animals-gallery-item">
-					<a class="gallery-item__img" href=""></a>
-					<img class="gallery-item__img" src="/images/fauna_rt/goose.png" alt="">
-					<p class="gallery-item__description">Гусь</p>
+					<a class="gallery-item__img" href="hunt#goose">
+						<img class="gallery-item__img" src="/images/fauna_rt/goose.png" alt="">
+						<p class="gallery-item__description">Гусь</p>
+					</a>
 				</div>
 
 				<div class="animals-gallery-item">
-					<a class="gallery-item__img" href=""></a>
-					<img class="gallery-item__img" src="/images/fauna_rt/duck.png" alt="">
-					<p class="gallery-item__description">Утка</p>
+					<a class="gallery-item__img" href="hunt#duck">
+						<img class="gallery-item__img" src="/images/fauna_rt/duck.png" alt="">
+						<p class="gallery-item__description">Утка</p>
+					</a>
 				</div>
 
 				<div class="animals-gallery-item">
-					<a class="gallery-item__img" href=""></a>
-					<img class="gallery-item__img" src="/images/fauna_rt/fox.png" alt="">
-					<p class="gallery-item__description">Лиса</p>
+					<a class="gallery-item__img" href="hunt#fox">
+						<img class="gallery-item__img" src="/images/fauna_rt/fox.png" alt="">
+						<p class="gallery-item__description">Лиса</p>
+					</a>
 				</div>
 
 				<div class="animals-gallery-item">
-					<a class="gallery-item__img" href=""></a>
-					<img class="gallery-item__img" src="/images/fauna_rt/beaver.png" alt="">
-					<p class="gallery-item__description">Бобр</p>
+					<a class="gallery-item__img" href="hunt#beaver">
+						<img class="gallery-item__img" src="/images/fauna_rt/beaver.png" alt="">
+						<p class="gallery-item__description">Бобр</p>
+					</a>
 				</div>
 
 				<div class="animals-gallery-item">
-					<a class="gallery-item__img" href=""></a>
-					<img class="gallery-item__img" src="/images/fauna_rt/gopher.png" alt="">
-					<p class="gallery-item__description">Суслик</p>
+					<a class="gallery-item__img" href="hunt#gopher">
+						<img class="gallery-item__img" src="/images/fauna_rt/gopher.png" alt="">
+						<p class="gallery-item__description">Суслик</p>
+					</a>
 				</div>
 			</div>
 		</div>
--- frontend/views/site/info.php
+++ frontend/views/site/info.php
@@ -5,10 +5,72 @@
 $this->title = 'Страница информации';
 ?>
 <div class="site-info">
+	<div class="title_img">
+		<div class="content">
+			<h2 class="title__discr">Полезная информация</h2>
+		</div>
+	</div>
 
-	<div class="body-content">
-		<div class="row">
-			Страница информации
+	<div class="content">
+		<div class="info-block-title">
+			<h2 class="info-block-title__name">Законодательные документы</h2>
+		</div>
+
+		<div class="blg">
+			<div class="blg__img"></div>
+			<div class="blg__text">
+				<ul>
+					<li><a href="https://docs.cntd.ru/document/9034380" target="_blank">Закон об оружии</a></li>
+					<li><a href="https://docs.cntd.ru/document/901729750" target="_blank">Правила по технике безопасности на охоте и обращению с охотничьим оружием</a></li>
+					<li><a href="https://docs.cntd.ru/document/9028679?marker=1I889EJ&section=text" target="_blank">О порядке приобретения, перевозки, хранения, ношения, учёта и использования служебного гражданского оружия и боеприпасов, состоящих на вооружении работников системы Федеральной службы лесного хозяйства России (РОСЛЕСХОЗА)</a></li>
+					<li><a href="https://docs.cntd.ru/document/901713364?marker=NVCA5M&section=text" target="_blank">Правила перевозки оружия (личный автомобиль, метро, самолёт)</a></li>
+					<li><a href="https://docs.cntd.ru/document/565612496" target="_blank">Правила охоты в Российской Федерации</a></li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="info-block">
+			<div class="info-menu">
+				<div class="info-menu-block">
+					<a class="info-menu__btn1 button" target="_blank"
+					   href="https://ojm.tatarstan.ru/kalendar-ohotnika-tatarstan-2020-sroki-ohoti-na.htm">КАЛЕНДАРЬ
+						ОХОТНИКА ТАТАРСТАН 2021</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="info-block-title">
+			<h2 class="info-block-title__name">Документы</h2>
+		</div>
+
+		<div class="documents-block">
+			<div class="document-item">
+				<a class="document-item__img" href="" target="_blank">
+					<img src="/images/pdf-format.png" alt="">
+					<p>Сообщение о продолжении деятельности РОО охотников РТ «Трофей» (2017)</p>
+				</a>
+			</div>
+
+			<div class="document-item">
+				<a class="document-item__img" href="" target="_blank">
+					<img src="/images/pdf-format.png" alt="">
+					<p>Отчёт в Управление Минюста России по Республике Татарстан РОО охотников РТ «Трофей» (2017)</p>
+				</a>
+			</div>
+
+			<div class="document-item">
+				<a class="document-item__img" href="" target="_blank">
+					<img src="/images/pdf-format.png" alt="">
+					<p>Сообщение о продолжении деятельности РОО охотников РТ «Трофей» (2018)</p>
+				</a>
+			</div>
+
+			<div class="document-item">
+				<a class="document-item__img" href="" target="_blank">
+					<img src="/images/pdf-format.png" alt="">
+					<p>Отчёт в Управление Минюста России по Республике Татарстан РОО охотников РТ «Трофей» (2018)</p>
+				</a>
+			</div>
 		</div>
 	</div>
 </div>
